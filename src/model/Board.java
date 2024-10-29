565928b878ed268d0e6a97ff3b5370f76bb54eb8
manish.tawade98@gmail.com <manish.tawade98@gmail.com>
2024-10-29 14:06:13 +0000

    Simplified checker creation logic by adding a method to do the same. Changes done in Board class, in initializeBoard method and a new method - createCheckers created

--- src/model/Board.java
+++ src/model/Board.java
@@ -12,14 +12,31 @@ public class Board {
 
     // Initialize the board with checkers in starting positions
     private void initializeBoard() {
-        positions.put(1, new Checker[]{new Checker("Black"), new Checker("Black")});
+        /*positions.put(1, new Checker[]{new Checker("Black"), new Checker("Black")});
         positions.put(6, new Checker[]{new Checker("White"), new Checker("White"), new Checker("White"), new Checker("White"), new Checker("White")});
         positions.put(8, new Checker[]{new Checker("White"), new Checker("White"), new Checker("White")});
         positions.put(12, new Checker[]{new Checker("Black"), new Checker("Black"), new Checker("Black"), new Checker("Black"), new Checker("Black")});
         positions.put(13, new Checker[]{new Checker("White"), new Checker("White"), new Checker("White"), new Checker("White"), new Checker("White")});
         positions.put(17, new Checker[]{new Checker("Black"), new Checker("Black"), new Checker("Black")});
         positions.put(19, new Checker[]{new Checker("Black"), new Checker("Black"), new Checker("Black"), new Checker("Black"), new Checker("Black")});
-        positions.put(24, new Checker[]{new Checker("White"), new Checker("White")});
+        positions.put(24, new Checker[]{new Checker("White"), new Checker("White")});*/
+        positions.put(1, createCheckers("White", 2));
+        positions.put(6, createCheckers("Black", 5));
+        positions.put(8, createCheckers("Black", 3));
+        positions.put(12, createCheckers("White", 5));
+        positions.put(13, createCheckers("Black", 5));
+        positions.put(17, createCheckers("White", 3));
+        positions.put(19, createCheckers("White", 5));
+        positions.put(24, createCheckers("Black", 2));
+    }
+
+    //added to simplify the checker create logic
+    private Checker[] createCheckers(String color, int count) {
+        Checker[] checkers = new Checker[count];
+        for (int i = 0; i < count; i++) {
+            checkers[i] = new Checker(color);
+        }
+        return checkers;
     }
 
     // Helper method to get the display symbol for a checker
